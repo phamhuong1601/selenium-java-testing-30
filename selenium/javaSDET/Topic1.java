cc6a1735e703c8e352353645833d154f4c7e8110
phamhuong1601 <huongpt1604@gmail.com>
2025-05-20 04:21:05 +0700

    second commit

--- selenium/javaSDET/Topic1.java
+++ selenium/javaSDET/Topic1.java
@@ -3,7 +3,7 @@ package javaSDET;
 public class Topic1 {
 public static void main(String[] args)
 {
-    System.out.println("Hello word my project");
+    System.out.println("Hello word my project huongpt");
 }
 
 }
